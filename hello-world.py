0a7d5ec3c87b8f8abfa2041c2b97d8b28b3e955c
Bahodir01I <ismoilovbakhodir653@gmail.com>
2026-02-19 14:59:02 +0500

    First file added
    Hello world

---
+++ hello-world.py
@@ -0,0 +1,35 @@
+# print("hello world")
+# print('Assalomu aleykum')
+# print(2+4*2)
+# print(19/5)
+# print(2**4)
+
+
+#3 - dars
+
+# print("Hayrli tong")
+# print('Men "HP" markasidagi notebook sotib oldim')
+# print("Men \"HP\" markasidagi notebook sotib oldim")  #black slash
+# print("""Oltin olma,
+# duo ol""")  # matnni ikki qatorga bo'lish
+# print("""Oltin \nolma, \nduo ol""")
+
+
+
+# print(2+4*2)
+# print(19/5)
+# print(20/5)
+# print(16//4)
+# print(15/4)
+# print(15//4)
+# print(2**4)
+# print(5**2)
+# print("oltining kvadrati", 6**2, "ga teng")
+#print('3*3=',3*3)
+
+#print('"Nexia", "Tico", "Damas", "ko\'rganlar qilar havas"')
+# print('5 ning 4-darajasi', 5**4)
+# print(22%4)
+# print("Tomonlari 125 ga teng kvadratni yuzi", 125*125, "ga, peremetri", 4*125, "ga teng" )
+# print("Diametri 12 teng bo'lgan doiraning yuzi", 3.14*(12//2)**2)
+# print("Katetlari 6 va 7 ga teng bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
